eadb7065a74311ff3e9918faf74c0b43f89d291a
lmatkows <lmatkows@student.42perpignan.fr>
2026-06-09 15:29:46 +0200

    ex03 terminado

--- ex03/ft_calculator.py
+++ ex03/ft_calculator.py
@@ -1,13 +1,34 @@
 class calculator:
     """A class that is able to do calculations of a vector with a scalar"""
 
-    vector: list = []
+
+    def __init__(self, vector):
+        """Stores the value of the vector"""
+        self.vector = vector
+
 
     def __add__(self, object) -> None:
-        """Addition"""
-    #your code here
+        """Adds the scalar to the vector"""
+        self.vector = [nb + object for nb in self.vector]
+        print(self.vector)
+
+
     def __mul__(self, object) -> None:
-    #your code here
+        """Multiplies the scalar with the vector"""
+        self.vector = [nb * object for nb in self.vector]
+        print(self.vector)
+
+
     def __sub__(self, object) -> None:
-    #your code here
+        """Substracts the scalar from the vector"""
+        self.vector = [nb - object for nb in self.vector]
+        print(self.vector)
+
     def __truediv__(self, object) -> None:
+        """Divide the vector by the scalar"""
+        try:
+            assert object != 0
+            self.vector = [nb / object for nb in self.vector]
+            print(self.vector)
+        except AssertionError:
+            print("AssertionError: Division by zero")
